d99880fe741be6c84b3576f36bcf4cb275eb535f
ip.osinga <ip.osinga@alfa-college.nl>
2023-09-29 10:24:31 +0200

    v2023-09-29

--- tafelsoefenen/script.js
+++ tafelsoefenen/script.js
@@ -1,22 +1,27 @@
+let tableof = 2.5
+let tabletimes = 3.3
+
+
 function displayQuestion()
 {
-    let tafelvan = document.getElementById("inputTafelVan").value
-    let tafellengte = document.getElementById("inputTafelLengte").value
-    let tafeltekst = "" //een lege string, wordt uiteindelijk de tekst die op het scherm komt
-//let teller = 1 //een teller die bij gaat houden 1 x7, 2 x7 3 x7 etc.
-    let uitkomst = 0 //de uitkomst van onze berekeningen
-    //while (teller <= tafellengte)
-    for (let teller = 1; teller <= tafellengte; teller++)
-    {
-        uitkomst = teller * tafelvan
-        tafeltekst += teller + " x " + tafelvan + " = " + uitkomst + "<br>"
-        //hier moet iets komen als: als de teller 10, 20, 30 etc. is, dan moet er een extra <br> geplaatst worden
-        //je gaat een if-statement maken die checkt op deelbaarheid van 10
-        //in alle programmeertalen is daar een operator voor:
-        if(teller % 10 == 0)
-            tafeltekst += "<br>"
-        //teller++ //de teller wordt 1 groter aan het einde van de lus
-    }
-    document.getElementById("tafel").innerHTML = tafeltekst
-    document.getElementById("tafelheader").innerHTML = "Tafel van " + tafelvan
+    let opgtekst = ""
+    let tafeltot = document.getElementById("inputtafellengte").value
+    let tafelvan = document.getElementById("inputtafelvan").value
+    tableof = tafelvan
+    //ik moet een random getal hebben tussen 1 en tafeltot (ze mogen zelf beide ook)
+    let randomnumber = Math.floor(Math.random() * tafeltot) + 1
+    tabletimes = randomnumber
+    //let randomnumber = Math.ceil(Math.random() * tafeltot)
+    opgtekst = randomnumber + " x " +  tafelvan
+    document.getElementById("inputsom").value = opgtekst
+}
+
+function checkAnswer()
+{
+    let answer = document.getElementById("inputantwoord").value
+    let correctanswer = tableof * tabletimes
+    if(answer == correctanswer)
+        alert ("GOED")
+    else
+        alert ("FOUT")
 }
